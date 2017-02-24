ef27006c7234d567dceb29bd788e127525f821c6
knightxy <mortimerjava@gmail.com>
2017-02-24 16:34:07 +0800

    设置头像点击事件

--- SmartBulter/app/src/main/java/com/basic/xy/smartbulter/fragment/UserFragment.java
+++ SmartBulter/app/src/main/java/com/basic/xy/smartbulter/fragment/UserFragment.java
@@ -1,9 +1,15 @@
 package com.basic.xy.smartbulter.fragment;
 
-
+import android.Manifest;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.BuildConfig;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.FileProvider;
+import android.support.v4.os.ResultReceiver;
 import android.text.TextUtils;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -19,12 +25,21 @@ import com.basic.xy.smartbulter.R;
 import com.basic.xy.smartbulter.activity.LoginActivity;
 import com.basic.xy.smartbulter.entity.MyUser;
 import com.basic.xy.smartbulter.util.L;
+import com.basic.xy.smartbulter.util.UtilToools;
 import com.basic.xy.smartbulter.view.CustomDialog;
+
+import java.io.File;
+import java.io.IOException;
+import java.security.Permission;
+import java.security.Permissions;
 
 import cn.bmob.v3.BmobUser;
 import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.http.bean.Result;
 import cn.bmob.v3.listener.UpdateListener;
 import de.hdodenhof.circleimageview.CircleImageView;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -39,6 +54,14 @@ public class UserFragment extends Fragment {
     private CircleImageView mAvatar;
     private CustomDialog dialog;
     private Button mTakePhoto,mGallery,mCancel;
+
+    public static final String AVATAR_NAME = "avatar.jpg";
+    private File tempFile = null;
+    public static final int REQUEST_TAKE_PHOTO = 101;
+    public static final int REQUEST_GALLERY = 102;
+    public static final int REQUEST_CROP = 103;
+    private static int MY_PERMISSIONS_REQUEST_CAMERA = 1111;
+
     public UserFragment() {
         // Required empty public constructor
     }
@@ -48,6 +71,7 @@ public class UserFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+//        UtilToools.requestPermission(new String[]{Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE},getActivity(),MY_PERMISSIONS_REQUEST_CAMERA);
         layout = inflater.inflate(R.layout.fragment_user, container, false);
         initVariable();
         initView();
@@ -113,11 +137,16 @@ public class UserFragment extends Fragment {
         mTakePhoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                onTakePhoto();
             }
         });
         mGallery = (Button) dialog.findViewById(R.id.btn_gallery);
-
+        mGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onGallery();
+            }
+        });
         mCancel = (Button) dialog.findViewById(R.id.btn_cancel_dialog);
         mCancel.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -211,5 +240,65 @@ public class UserFragment extends Fragment {
         }
     }
 
+    private void onGallery() {
+        dialog.dismiss();
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent,REQUEST_GALLERY);
+    }
 
+    private void onTakePhoto() {
+        dialog.dismiss();
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File file = new File(Environment.getExternalStorageDirectory(), AVATAR_NAME);
+        file.mkdir();
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Uri uri = FileProvider.getUriForFile(getActivity(), "com.basic.xy.smartbulter.fileprovider", file);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+        startActivityForResult(intent,REQUEST_TAKE_PHOTO);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case REQUEST_TAKE_PHOTO:
+                    tempFile = new File(Environment.getExternalStorageDirectory(), AVATAR_NAME);
+                    Uri uri = FileProvider.getUriForFile(getActivity(), "com.basic.xy.smartbulter.fileprovider", tempFile);
+                    cropPhoto(uri);
+                    break;
+                case REQUEST_GALLERY:
+                    cropPhoto(data.getData());
+                    break;
+                case REQUEST_CROP:
+
+                    break;
+            }
+        } else {
+            return;
+        }
+    }
+
+    private void cropPhoto(Uri uri) {
+        if (uri == null) {
+            L.e("uri == null");
+            return;
+        }
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setDataAndType(uri, "image/*");
+        //设置裁剪
+        intent.putExtra("crop", "true");
+        //裁剪宽高比例
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+        //设置图片的质量
+        intent.putExtra("outputX", 320);
+        intent.putExtra("outputY", 320);
+        startActivityForResult(intent,REQUEST_CROP);
+    }
 }
--- SmartBulter/app/src/main/java/com/basic/xy/smartbulter/util/UtilToools.java
+++ SmartBulter/app/src/main/java/com/basic/xy/smartbulter/util/UtilToools.java
@@ -1,7 +1,12 @@
 package com.basic.xy.smartbulter.util;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.Typeface;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.widget.TextView;
 
 /**
@@ -21,5 +26,32 @@ public class UtilToools {
     public static void setFont(Context mContext, TextView mTextView) {
         Typeface fontType = Typeface.createFromAsset(mContext.getAssets(), "fonts/FONT.TTF");
         mTextView.setTypeface(fontType);
+    }
+
+    public static void requestPermission(String[] permission,Activity activity,int request_code) {
+        for (String p : permission) {
+            if (ContextCompat.checkSelfPermission(activity,
+                   p)
+                    != PackageManager.PERMISSION_GRANTED) {
+                // Should we show an explanation?
+                if (ActivityCompat.shouldShowRequestPermissionRationale(activity,
+                        p)) {
+                    // Show an expanation to the user *asynchronously* -- don't block
+                    // this thread waiting for the user's response! After the user
+                    // sees the explanation, try again to request the permission.
+                    ActivityCompat.requestPermissions(activity,
+                            new String[]{p},
+                            request_code);
+                } else {
+                    // No explanation needed, we can request the permission.
+                    ActivityCompat.requestPermissions(activity,
+                            new String[]{p},
+                            request_code);
+                    // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                    // app-defined int constant. The callback method gets the
+                    // result of the request.
+                }
+            }
+        }
     }
 }
